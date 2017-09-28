93ff657fee18260756bea2b507f78582fb609104
= <=>
2017-09-28 14:07:14 +0200

    Added db creation and image list

--- image_crawler.py
+++ image_crawler.py
@@ -1,28 +1,41 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 29 10:53:14 2016
+#!/usr/bin/env python
+#title:		image_crawler.py
+#description:	Download the last public images from Flickr with associated information
+#author:		Alessandro Ortis
+#date:		20170928
+#version:		0.1
+#usage:		python image_crawler.py 10000  --> download up to 10000 new images and update the daily information of known images
+#               python image_crawler.py         --> only update existing images
+#notes:         if there are images in the DB, update daily information
+#==============================================================================
 
-@author: alessandro
-"""
 
 import flickrapi
 import json
+import pickle
 import urllib, urllib2
 import sqlite3 as lite
 import csv
-import sys, os
+import sys, os, os.path
 import time
 from skimage.measure import structural_similarity as ssim
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+from sys import argv
+import sqlite3 as lite
+
+
 """
     Settings:
         
         -   Flickr API Settings
-        -   Dataset Settings
+        -   DB names
 
 """
+# Null image name
+referenceImage="flickrMissing"
+
 # Credenziali The Social Picture
 api_key = u'c3388aa658417c77dcc98d5f9dc3ac91'
 api_secret = u'9f3693a3ab57bfc1'
@@ -31,15 +44,35 @@ api_secret = u'9f3693a3ab57bfc1'
 api_key= u'394b14cc54cd9cba5aa0d68b1d5f7eb9'
 api_secret = u'9bda221d1de625a9'
 
-# create Flickr image pool from data
-dataset_dir = "/home/alessandro/CrossSentiment/FlickrDatasetKatsurai/"
-project_root_dir = "/home/alessandro/flickrCrawling/"
-image_pool = []
+# Le seguenti due righe sono state eseguite una volta sola per generare il token di autorizzazione    
+#flickr = flickrapi.FlickrAPI(api_key, api_secret)
+#flickr.authenticate_via_browser(perms='read')
+oauth_token=u'72157675663685032-6052b3ccbdc594e8'
+oauth_verifier=u'1103dab7cc7d9c5e'
+
 
 
 
 """ END SETTINGS """
 
+def create_db(db_name, tab_name, query, drop_existing = False):
+    con = lite.connect(db_name)
+    cur = con.cursor()
+    if(drop_existing):
+        q = "DROP TABLE IF EXISTS " + tab_name
+        cur.execute(q)
+    # Create empty table
+    try:
+        cur.execute(query)
+        print "Creating a new DB/table\t" + tab_name + "\n" 
+    except Exception, e:
+        if not str(e) == 'table ' + tab_name + ' already exists':
+            print "Error:\t" + str(e)
+        else:
+            print str(e)
+    con.close()
+ 
+ 
 def find_by_size(size,path):
     result = []
     for root, dirs, files in os.walk(path):
@@ -82,7 +115,6 @@ def createRecord(csvrow):
 
     return record    
 
-referenceImage="flickrMissing"
 
 def mse(A,B):
     err = np.sum((A.astype("float") - B.astype("float")) ** 2)
@@ -118,13 +150,108 @@ def isMissing(imagePath):
 
     plt.show()    
     """
-# Le seguenti due righe sono state eseguite una volta sola per generare il token di autorizzazione    
-#flickr = flickrapi.FlickrAPI(api_key, api_secret)
-#flickr.authenticate_via_browser(perms='read')
-oauth_token=u'72157675663685032-6052b3ccbdc594e8'
-oauth_verifier=u'1103dab7cc7d9c5e'
 
-# Crea il pool di immagini del dataset di Katsurai
+
+
+
+##
+## Load list of known images, or create a new one
+##
+
+# Keeps the list of known images
+known_images = []
+pickle_image_list = 'known_images.pickle'
+
+if not os.path.isfile(pickle_image_list):
+    print('Creating (empty) image list file (pickle)')
+    data = []
+    with open(pickle_image_list,'wb') as f:
+        pickle.dump(data,f)
+else:
+    print('Loading image list from pickle')
+    with open(pickle_image_list,'r') as f:
+        known_images = pickle.load(f)
+        
+if len(argv)>1:
+    new_images_count = int(argv[1])
+    print "Attempting to download: %s new images" % (new_images_count)
+else:
+    print "The script will not download new images."
+##-----------------------------------------------------------------
+    
+##
+## Create DBs
+##
+headers_db      = 'headers.db'      #mage id, user id, groups ids
+image_info_db   = 'image_info.db'   #image meta-data
+image_daily_db  = 'image_daily.db'  #daily information
+user_info_db    = 'user_info.db'    #user information
+groups_info_db  = 'groups_info.db'  #groups information
+
+t_name = 'headers'
+query = "CREATE TABLE "+t_name+"(Id INTEGER PRIMARY KEY, \
+							FlickrId TEXT, \
+							UserId TEXT, \
+							GroupsIds TEXT, \
+							DateCrawl TEXT)"
+
+create_db(headers_db, t_name, query, drop_existing = False)
+
+
+t_name = 'image_info'
+query = "CREATE TABLE "+t_name+"(Id INTEGER PRIMARY KEY, \
+							FlickrId TEXT, \
+							Day TEXT, \
+							Title TEXT, \
+							Description TEXT, \
+                                       Tags TEXT)"
+
+create_db(image_info_db, t_name, query, drop_existing = False)
+
+
+t_name = 'image_daily'
+query = "CREATE TABLE "+t_name+"(Id INTEGER PRIMARY KEY, \
+							FlickrId TEXT, \
+							Day TEXT, \
+							field1 TEXT, \
+							field2 TEXT, \
+                                       field3 TEXT)"
+
+create_db(image_daily_db, t_name, query, drop_existing = False)
+
+
+t_name = 'user_info'
+query = "CREATE TABLE "+t_name+"(Id INTEGER PRIMARY KEY, \
+							UserId TEXT, \
+							Day TEXT, \
+							field1 TEXT, \
+							field2 TEXT, \
+                                       field3 TEXT)"
+
+create_db(user_info_db, t_name, query, drop_existing = False)
+
+t_name = 'groups_info'
+query = "CREATE TABLE "+t_name+"(Id INTEGER PRIMARY KEY, \
+							GroupId TEXT, \
+							Day TEXT, \
+							field1 TEXT, \
+							field2 TEXT, \
+                                       field3 TEXT)"
+
+create_db(groups_info_db, t_name, query, drop_existing = False)
+##-----------------------------------------------------------------
+sys.exit(0)
+
+
+"""
+TODO: 
+-   image_info aggiornato giornalmente (un record per ogni giorno per ogni immagine).
+-   gli altri DB sia aggiornano solo se cambia effettivamente il dato: per ogni dato tendenzialmente fisso, verificare se cambia rispetto l'ultima volta.
+
+"""
+
+sys.exit(0)
+
 
 for i in range(3):
     filename = "flickr" + str(i+1) + "_icassp2016_dataset.csv"
